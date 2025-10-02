283be0d48082ab500848a763654f1d6d5816d751
Raheb Saeed <raheebareef@gmail.com>
2025-10-02 13:14:35 +0100

    Add __main__.py to provide script execution entry point

---
+++ treef/__main__.py
@@ -0,0 +1,11 @@
+# treef/__main__.py
+
+"""
+This file allows the package to be executed as a script with `python -m treef`.
+It acts as an alternative entry point to the console script.
+"""
+
+from .cli import main
+
+if __name__ == "__main__":
+    main()
